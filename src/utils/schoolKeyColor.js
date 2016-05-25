4f6e8cc2dcb0cbb173a0057ae1e8f61d6a86a360
yalmadani <ynmadani@gmail.com>
2016-05-25 10:16:17 +0300

    Added Colors

--- src/utils/schoolKeyColor.js
+++ src/utils/schoolKeyColor.js
@@ -1,34 +1,38 @@
 export const schoolKeyColor = {
-    0: {
-        fillColor: "rgba(255,100,100,.7)",
-        strokeColor: "rgba(255,220,220,1)",
+    //Teal
+    {
+        fillColor: "rgba(26, 188, 156, 0.1)",
+        strokeColor: "rgb(26, 188, 156)",
         pointColor: "rgba(255,220,220,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)"
     },
-    1: {
-        fillColor: "rgba(255,100,100,.7)",
-        strokeColor: "rgba(255,220,220,1)",
+    //Red
+    {
+        fillColor: "rgba(231, 76, 60, 0.1)",
+        strokeColor: "rgb(231, 76, 60)",
         pointColor: "rgba(255,220,220,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)"
     },
-    2: {
-        fillColor: "rgba(255,100,100,.7)",
-        strokeColor: "rgba(255,220,220,1)",
+    //Orange
+    {
+        fillColor: "rgba(52, 152, 219, 0.1)",
+        strokeColor: "rgb(52, 152, 219)",
         pointColor: "rgba(255,220,220,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)"
-    }, 
-    3: {
-        fillColor: "rgba(255,100,100,.7)",
-        strokeColor: "rgba(255,220,220,1)",
+    },
+    //Blue
+    {
+        fillColor: "rgba(96, 125, 139, 0.1)",
+        strokeColor: "rgb(96,125,139)",
         pointColor: "rgba(255,220,220,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)"
-    }
+    },
+    //Purple
+    {
+        fillColor: "rgba(156, 39, 176, 0.1)",
+        strokeColor: "rgb(156,39,176)",
+        pointColor: "rgba(255,220,220,1)",
+    },
+    //Yellow
+    {
+        fillColor: "rgba(255, 235, 59, 0.1)",
+        strokeColor: "rgb(255,235,59)",
+        pointColor: "rgba(255,220,220,1)",
+    },
 }
