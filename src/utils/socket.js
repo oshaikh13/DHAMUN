96d8650fdbd4cc8a575f9e641a08d6542908528e
Omar Shaikh <oshaikh13@gmail.com>
2016-07-19 23:54:42 -0400

    Add Socket.io stub and change token response

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,15 @@
 import io from 'socket.io-client';
 
-let socket = io(SERVER_URL);
+let socket;
+
+if (typeof SERVER_URL !== "undefined") {
+  socket = io(SERVER_URL);
+} else {
+  socket = {
+    emit: function (event, object) {
+      // stub
+    }
+  }
+}
 
 export { socket };
