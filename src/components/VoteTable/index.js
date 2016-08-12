a30c403f0303d59c7181864d71784dd7eb02333d
Omar Shaikh <oshaikh13@gmail.com>
2016-08-12 16:13:23 -0400

    Fix button shift

--- src/components/VoteTable/index.js
+++ src/components/VoteTable/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { Button } from 'react-toolbox/lib/button';
+
+import { styles } from '../ResolutionTable/styles.scss';
+
 
 export class VoteTable extends Component {
 
@@ -16,15 +20,17 @@ export class VoteTable extends Component {
             
           </tr>
         </thead>
-        <tbody>
+        <tbody className={styles} >
 
           {
             Object.keys(votes).map((item, index) => 
               <tr key={index}>
-                <td>{item}</td>
-                <td>{votes[item].closed ? "Closed" : "Open"}</td>
-                <td>{votes[item].creator}</td>
-                <td><a type="button" className="btn btn-primary" href={'/#/dashboard/' + this.props.userLevel + '/vote/' + encodeURIComponent(item)}>Vote</a></td>
+                <td className="col-md-3">{item}</td>
+                <td className="col-md-3">{votes[item].closed ? "Closed" : "Open"}</td>
+                <td className="col-md-3">{votes[item].creator}</td>
+                <td className="col-md-3">
+                  <Button label="Vote" className="btn centerMe" href={'/#/dashboard/' + this.props.userLevel + '/vote/' + encodeURIComponent(item)} primary/>
+                </td>
               </tr>
             )
           }
